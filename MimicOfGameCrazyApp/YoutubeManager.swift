0a70141511152fbf3b96d7784341df0bd13e3582
michaelrevlis <michaelrevlis@gmail.com>
2016-11-30 15:44:23 +0800

    create TabBarController, TableViewControllers and YoutubeManager

---
+++ MimicOfGameCrazyApp/YoutubeManager.swift
@@ -0,0 +1,14 @@
+//
+//  YoutubeManager.swift
+//  MimicOfGameCrazyApp
+//
+//  Created by MichaelRevlis on 2016/11/30.
+//  Copyright © 2016年 MichaelRevlis. All rights reserved.
+//
+
+import Foundation
+
+
+class YoutuberManager {
+    
+}
